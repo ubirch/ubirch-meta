2305d49cca0c9f0d390b93c95235b8b020cabf7e
Matthias L. Jugel <leo@thinkberg.com>
2016-05-19 12:40:03 +0200

    build for all confis and board types

--- build.sh
+++ build.sh
@@ -2,12 +2,19 @@
 # do an out-of-source build for all configurations
 if [ "$1" == "-u" ]; then export UPDATE="1"; shift; fi
 if [ "$1" == "-a" ]
-then BUILDS="RelWithDebInfo MinSizeRel Release Debug"
-else BUILDS=MinSizeRel
+then
+  BOARDS="ubirch-1 FRDM-K82F"
+  BUILDS="RelWithDebInfo MinSizeRel Release Debug"
+else
+  BOARDS="ubirch-1"
+  BUILDS="MinSizeRel"
 fi
 for BUILD_TYPE in $BUILDS
 do
-  mkdir -p build/$BUILD_TYPE
-  (cd build/$BUILD_TYPE; cmake ../.. -DCMAKE_BUILD_TYPE=$BUILD_TYPE; make)
-  unset UPDATE
+  for BOARD in $BOARDS
+  do
+    mkdir -p build/$BUILD_TYPE
+    (cd build/$BUILD_TYPE; cmake ../.. -DBOARD=$BOARD -DCMAKE_BUILD_TYPE=$BUILD_TYPE; make)
+    unset UPDATE
+  done
 done
