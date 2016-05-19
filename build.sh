a7fd8632378b44b326ef15029bd293fc695c6138
Matthias L. Jugel <leo@thinkberg.com>
2016-05-19 15:46:29 +0200

    fix build process for multi-config

--- build.sh
+++ build.sh
@@ -2,19 +2,12 @@
 # do an out-of-source build for all configurations
 if [ "$1" == "-u" ]; then export UPDATE="1"; shift; fi
 if [ "$1" == "-a" ]
-then
-  BOARDS="ubirch-1 FRDM-K82F"
-  BUILDS="RelWithDebInfo MinSizeRel Release Debug"
-else
-  BOARDS="ubirch-1"
-  BUILDS="MinSizeRel"
+then BUILDS="RelWithDebInfo MinSizeRel Release Debug"
+else BUILDS="MinSizeRel"
 fi
 for BUILD_TYPE in $BUILDS
 do
-  for BOARD in $BOARDS
-  do
-    mkdir -p build/$BUILD_TYPE
-    (cd build/$BUILD_TYPE; cmake ../.. -DBOARD=$BOARD -DCMAKE_BUILD_TYPE=$BUILD_TYPE; make)
-    unset UPDATE
-  done
+  mkdir -p build/$BUILD_TYPE
+  (cd build/$BUILD_TYPE; cmake ../.. -DCMAKE_BUILD_TYPE=$BUILD_TYPE; make)
+  unset UPDATE
 done
