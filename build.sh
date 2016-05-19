4a56c34dd1b1bdcd8eb6fdc20508a75bcba8cc85
Matthias L. Jugel <leo@thinkberg.com>
2016-05-19 09:08:34 +0200

    fix path and build

--- build.sh
+++ build.sh
@@ -1,17 +1,13 @@
 #! /bin/sh
 # do an out-of-source build for all configurations
-if [ "$1" == "-u" ]; then UPDATE="1"; shift; fi
+if [ "$1" == "-u" ]; then export UPDATE="1"; shift; fi
 if [ "$1" == "-a" ]
 then BUILDS="RelWithDebInfo MinSizeRel Release Debug"
 else BUILDS=MinSizeRel
 fi
 for BUILD_TYPE in $BUILDS
 do
-  if [ -d build/$BUILD_TYPE ]
-  then
-    (cd build/$BUILD_TYPE; make)
-  else
-    mkdir -p build/$BUILD_TYPE
-    (cd build/$BUILD_TYPE; cmake ../.. -DCMAKE_BUILD_TYPE=$BUILD_TYPE && make)
-  fi
+  mkdir -p build/$BUILD_TYPE
+  (cd build/$BUILD_TYPE; cmake ../.. -DCMAKE_BUILD_TYPE=$BUILD_TYPE; make)
+  unset UPDATE
 done
